27bf7f4b25558379df258f5850878205c670cdfd
Jun Jun Gabion <jbgabion@sscgi.com>
2024-11-26 15:56:36 +0800

    Initial commit

---
+++ discussion/index.js
@@ -0,0 +1,42 @@
+// console.log("Hello world!");
+
+// const submit = document.querySelector("#submit");
+
+// submit.addEventListener("click", function () {
+//   console.log(submit);
+// });
+
+// function printName() {
+//   console.log("Hello World!");
+// }
+// printName();
+// // function invocation the code code block and statement inside a function is not immediately defined. the conde block and statement inside a function is execute when the function is invoke or called. -- "call a function"
+
+// function hoisting function
+// greet();
+// function greet() {
+//   console.log("Hello World!");
+// }
+
+// Function Scoping
+// scope is the accessibility (visibility) of variables within our program.
+/*type score:
+-local/block scope
+-global scope
+-function scope
+
+*/
+
+// function showName() {
+//   function getName() {
+//     var name = "jun";
+//     console.log(name);
+//     return name;
+//   }
+// }
+// showName();
+//function parameters and arguments
+function printName(name) {
+  console.log("My name: " + name);
+}
+printName("Happy");
